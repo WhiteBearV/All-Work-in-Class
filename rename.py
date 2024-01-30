0883ebfb028e37f6adc0605e2a038341a3f1ce1e
WhiteBearV <kusunzaza096@gmail.com>
2024-01-30 22:14:34 +0700

    งานเปลี่ยนชื่อ;-)

---
+++ rename.py
@@ -0,0 +1,17 @@
+import os
+
+def Rename_file():
+    location = "D:\Meme" #เลือกไดร์
+    dir_list = os.listdir(location)
+    os.chdir(location)
+    i = 1
+
+    for filename in dir_list:
+        newname = "This_is_Picture"  # ชื่อที่จะนำมาใส่แทนชื่อเดิม
+        name1 = "{}_{:01d}.png".format(newname, i) #{:01d}ใช้กำหนดความยาวตัวเลข
+        os.rename(filename , name1)
+        
+        i +=1
+    print(f"\n\n\nChange_Nam_Succes\n   \m/*.*\m/\n\n\n")
+if __name__ == "__main__" :
+     Rename_file()
